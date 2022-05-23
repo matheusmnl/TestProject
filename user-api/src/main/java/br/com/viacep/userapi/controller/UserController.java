2bd2e4c242ede18cdb640c6e2c76cb95abd42490
matheusmnl <matheusricardo543@gmail.com>
2022-05-23 00:26:58 -0300

    JV01-RAFATORAÇÃO DO CONTROLLER CRIACAO DO CRUD

--- user-api/src/main/java/br/com/viacep/userapi/controller/UserController.java
+++ user-api/src/main/java/br/com/viacep/userapi/controller/UserController.java
@@ -1,9 +1,7 @@
 package br.com.viacep.userapi.controller;
 
 import br.com.viacep.userapi.dto.UserDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -26,13 +24,29 @@ public class UserController {
         }
         return null;
     }
+    @PostMapping("/newUser")
+    UserDTO inserir(@RequestBody UserDTO userDTO) {
+        userDTO.setDataCadastro(new Date());
+        usuarios.add(userDTO);
+        return userDTO;
+    }
+    @DeleteMapping("/user/{cep}")
+    public boolean remover(@PathVariable String cep){
+        for (UserDTO userFilter:usuarios) {
+            if (userFilter.getCep().equals(cep)) {
+                usuarios.remove(userFilter);
+                return true;
+            }
+        }
+        return false;
+    }
     public static List<UserDTO> usuarios = new ArrayList<UserDTO>();
         @PostConstruct
         public void initiateList() {
 
             UserDTO userDTO = new UserDTO();
             userDTO.setNome("Matheus Ricardo");
-            userDTO.setCep("59104-150");
+            userDTO.setCep("59104140");
             userDTO.setEmail("matheus@email.com");
             userDTO.setCpf("098.098.098-55");
             userDTO.setEndereco("Avenida tupi");
